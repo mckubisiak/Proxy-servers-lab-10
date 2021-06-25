8f4a8a65f4d9abc7f1b1b3d76fcd6830c4e4fd5e
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-25 09:51:57 -0700

    resolved function import issue missing brackets

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
